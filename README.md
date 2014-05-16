e45c85b260aba520a5531d4a561795902f0699d5
David Bau <david.bau@gmail.com>
2014-05-16 03:11:11 -0400

    Parse tempo markings.

--- README.md
+++ README.md
@@ -2,3 +2,30 @@ sequencer.js
 ============
 
 sequencer.js: a sequencing WebAudio synthesizer that supports ABC notation.
+
+Designed for use by jQuery-turtle.
+
+API:
+
+<pre>
+piano = new Instrument();
+
+// Play a single note.
+r = piano.tone('C')
+
+// Whenever we like, release the note.
+setTimeout(function() { r.release(); }, Math.random() * 1000);
+
+// Play "Mary Had a Little Lamb"
+piano.play("AGFG|AAA2|GGG2|AAA2|AGFG|AAAA|GGAG|F4|", whendone)
+
+function whendone() {
+  // Do this after Mary is done.
+  // Play "Stairway", which picks out a few chords.
+  g = new Instrument();
+  g.play("F^Gcf|[gE]c^G|g[^g^D]c|^G^g[dD]|" +
+         "^AFd|[^C=c]^GF|^G21/3c^GF|[G^DG,][F,F^G][^GFF,]2");
+
+}
+</pre>
+
--- instrument.js
+++ instrument.js
@@ -36,39 +36,34 @@ function parseOptionString(str, defaultProp) {
 var ABCtoken = /(?:^\[V:\S*\])|\s+|\[|\]|>+|<+|(?:(?:\^\^|\^|__|_|=|)[A-Ga-g](?:,+|'+|))|\d*\/\d+|\d+|\/+|[xzXZ]|\||%[^\n]*$|./g;
 var ABCheader = /^([A-Za-z]):\s*(.*)$/;
 var audioTop = null;
+
 function isAudioPresent() {
   return !!(window.AudioContext || window.webkitAudioContext);
 }
 function getAudioTop() {
   if (!audioTop) {
     var ac = new (window.AudioContext || window.webkitAudioContext),
-        dcn = ac.createDynamicsCompressor(),
         firstTime = null;
-    dcn.connect(ac.destination);
     audioTop = {
       ac: ac,
-      out: dcn,
-      // Partial workaround for http://crbug.com/254942:
-      // add little extra pauses before scheduling envelopes.
-      // A quarter second or so seems to be needed at initial startup,
-      // then 1/64 second before scheduling each envelope afterwards.
-      nextStartTime: function() {
-        if (firstTime === null) {
-          firstTime = ac.currentTime;
-        }
-        return Math.max(firstTime + 0.25,
-                        ac.currentTime + 0.015625);
-      }
+      out: null
     }
+    resetAudio();
   }
   return audioTop;
 }
 function resetAudio() {
-  if (audioTop && audioTop.out.disconnect) {
+  if (audioTop) {
     // Disconnect the top-level node and make a new one.
-    audioTop.out.disconnect();
-    audioTop.out = audioTop.ac.createDynamicsCompressor();
-    audioTop.out.connect(audioTop.ac.destination);
+    if (audioTop.out) {
+      audioTop.out.disconnect();
+      audioTop.out = null;
+    }
+    var dcn = audioTop.ac.createDynamicsCompressor();
+    dcn.ratio = 16;
+    dcn.attack = 0.0005;
+    dcn.connect(audioTop.ac.destination);
+    audioTop.out = dcn;
   }
 }
 // Returns a map of A-G -> accidentals, according to the key signature.
@@ -193,14 +188,25 @@ function parseABCFile(str) {
   for (j = 0; j < lines.length; ++j) {
     header = ABCheader.exec(lines[j]);
     if (header) {
-      if (header[1] == 'V') {
-        startVoiceContext(header[2].split(' ')[0]);
-      }
-      if (header[1] == 'I') {
-        timbre = /^timbre\s+(.*)$/.exec(header[2]);
-        if (timbre) {
-          context.timbre = parseTimbre(timbre);
-        }
+      switch(header[1]) {
+        case 'V':
+          startVoiceContext(header[2].split(' ')[0]);
+          break;
+        case 'M':
+          parseMeter(header[2], context);
+          break;
+        case 'L':
+          parseUnitNote(header[2], context);
+          break;
+        case 'Q':
+          parseTempo(header[2], context);
+          break;
+        case 'I':
+          timbre = /^timbre\s+(.*)$/.exec(header[2]);
+          if (timbre) {
+            context.timbre = parseTimbre(timbre);
+          }
+          break;
       }
       if (context.hasOwnProperty(header[1])) {
         context[header[1]] += '\n' + header[2];
@@ -214,7 +220,7 @@ function parseABCFile(str) {
     } else {
       out = {};
       stems = parseABCNotes(lines[j], key, accent, out);
-      if (stems.length) {
+      if (stems && stems.length) {
         startVoiceContext(out.voiceid);
         if (!('stems' in context)) { context.stems = []; }
         context.stems.push.apply(context.stems, stems);
@@ -230,6 +236,38 @@ function parseABCFile(str) {
     }
   }
   return result;
+}
+function parseMeter(mline, beatinfo) {
+  var d = durationToTime(mline);
+  if (!d) { return; }
+  if (!beatinfo.unitnote) {
+    if (d < 0.75) {
+      beatinfo.unitnote = 1/16;
+    } else {
+      beatinfo.unitnote = 1/8;
+    }
+  }
+}
+function parseUnitNote(lline, beatinfo) {
+  var d = durationToTime(lline);
+  if (!d) { return; }
+  beatinfo.unitnote = d;
+}
+function parseTempo(qline, beatinfo) {
+  var parts = qline.split(/\s*=\s*/), j, unit = null, tempo = null;
+  for (j = 0; j < parts.length; ++j) {
+    if (parts[j].indexOf('/') >= 0 || /^[1-4]$/.test(parts[j])) {
+      unit = unit || durationToTime(parts[j]);
+    } else {
+      tempo = tempo || Number(parts[j]);
+    }
+  }
+  if (unit) {
+    beatinfo.unitbeat = unit;
+  }
+  if (tempo) {
+    beatinfo.tempo = tempo;
+  }
 }
 function processTies(stems) {
   var tied = {}, nextTied, j, k, note, firstNote;
@@ -254,6 +292,9 @@ function processTies(stems) {
 function parseABCNotes(str, key, accent, out) {
   var tokens = str.match(ABCtoken), result = [], stem = null,
       index = 0, dotted = 0, t;
+  if (!tokens) {
+    return null;
+  }
   while (index < tokens.length) {
     if (/^s+$/.test(tokens[index])) { index++; continue; }
     if (/^\[V:\S*\]$/.test(tokens[index])) {
@@ -298,8 +339,8 @@ function parseABCNotes(str, key, accent, out) {
   return result;
 }
 function stripNatural(pitch) {
-  if (pitch.length > 0 && (!/[_=^]/.test(pitch.charAt(0)))) {
-    return '=' + pitch;
+  if (pitch.length > 0 && pitch.charAt(0) == '=') {
+    return pitch.substr(1);
   }
   return pitch;
 }
@@ -324,7 +365,7 @@ function applyAccent(pitch, key, accent) {
   return stripNatural(pitch);
 }
 function parseStem(tokens, index, key, accent) {
-  var note = [], frequency = [],
+  var note = [],
       duration = '', staccato = false;
   var lastNote = null, minStemTime = Infinity, j;
   if (index < tokens.length && '.' == tokens[index]) {
@@ -351,11 +392,6 @@ function parseStem(tokens, index, key, accent) {
       } else {
         break;
       }
-      // When a stem has more than one duration, select the
-      // shortest one. The standard says to pick the first one,
-      // but in practice, transcribed music online seems to
-      // follow the rule that the stem's duration is determined
-      // by the shortest contained duration.
       if (index < tokens.length && /\d|\//.test(tokens[index])) {
         noteDuration = tokens[index++];
         noteTime = durationToTime(noteDuration);
@@ -368,6 +404,11 @@ function parseStem(tokens, index, key, accent) {
         lastNote.duration = noteDuration;
         lastNote.time = noteTime;
       }
+      // When a stem has more than one duration, select the
+      // shortest one. The standard says to pick the first one,
+      // but in practice, transcribed music online seems to
+      // follow the rule that the stem's duration is determined
+      // by the shortest contained duration.
       if (noteTime && noteTime < minStemTime) {
         duration = noteDuration;
         minStemTime = noteTime;
@@ -412,31 +453,37 @@ function parseStem(tokens, index, key, accent) {
       note[j].tie = true;
     }
   }
-  frequency = []
-  for (j = 0; j < note.length; ++j) {
-    frequency.push(note[j].frequency);
-  }
   return {
     index: index,
     value: {
       note: note,
       duration: duration,
       staccato: staccato,
-      frequency: frequency,
       time: durationToTime(duration)
     }
   };
 }
-function pitchToFrequency(pitch) {
+function midiToFrequency(midi) {
+  return 440 * Math.pow(2, (midi - 69) / 12);
+}
+function frequencyToMidi(freq) {
+  // Approximate by getting the closest midi number.
+  return Math.round(69 + Math.log(freq / 440) * 12 / Math.LN2);
+}
+function pitchToMidi(pitch) {
   var m = /^(\^\^|\^|__|_|=|)([A-Ga-g])(,+|'+|)$/.exec(pitch);
   if (!m) { return null; }
   var n = {C:-9,D:-7,E:-5,F:-4,G:-2,A:0,B:2,c:3,d:5,e:7,f:8,g:10,a:12,b:14};
   var a = { '^^':2, '^':1, '': 0, '=':0, '_':-1, '__':-2 };
   var semitone = n[m[2]] + a[m[1]] + (/,/.test(m[3]) ? -12 : 12) * m[3].length;
-  return 440 * Math.pow(2, semitone / 12);
+  return semitone + 69; // 69 = midi code for "A", which is A4.
+}
+function pitchToFrequency(pitch) {
+  return midiToFrequency(pitchToMidi(pitch));
 }
 function durationToTime(duration) {
   var m = /^(\d*)(?:\/(\d*))?$|^(\/+)$/.exec(duration), n, d, i = 0, ilen;
+  if (!m) return;
   if (m[3]) return Math.pow(0.5, m[3].length);
   d = (m[2] ? parseFloat(m[2]) : /\//.test(duration) ? 2 : 1);
   // Handle mixed frations:
@@ -450,9 +497,9 @@ function durationToTime(duration) {
   return i + (n / d);
 }
 var pianoTimbre = parseOptionString(
-  "wave(sawtooth) volume(0.5) " +
-  "attack(0.001) decay(0.7) sustain(0.01) release(0.1) " +
-  "cutoff(2) resonance(1) detune(1.001)");
+  "wave(sawtooth) gain(0.25) " +
+  "attack(0.001) decay(0.3) sustain(0.01) release(0.1) " +
+  "cutoff(1) resonance(5) detune(1.001)");
 
 function parseTimbre(options) {
   if (!options) {
@@ -467,10 +514,11 @@ function Instrument(options) {
   this._timbre = parseTimbre(options);
   this._atop = getAudioTop();
   this._out = this._atop.ac.createGain();
-  this._out.gain.value = this.volume;
+  this._out.gain.value = this.gain;
   this._out.connect(this._atop.out);
   this._queue = [];
   this._minQueueTime = Infinity;
+  this._maxScheduledTime = 0;
   this._unsortedQueue = false;
   this._startSet = [];
   this._finishSet = [];
@@ -480,8 +528,12 @@ function Instrument(options) {
   this._now = null;
 }
 
-Instrument.bufferSecs = 3;   // Seconds ahead to buffer notes.
-Instrument.toneLength = 60;  // Default duration of a tone.
+Instrument.bufferSecs = 3;     // Seconds ahead to buffer notes.
+Instrument.toneLength = 60;    // Default duration of a tone.
+Instrument.cleanupDelay = 0.1; // Time before disconnecting gain nodes.
+Instrument.nowDelay = 0.02;    // Hack to avoid bug http://crbug.com/254942.
+
+Instrument.getAudioTop = getAudioTop;
 
 Instrument.prototype.silence = function() {
   var j;
@@ -502,7 +554,7 @@ Instrument.prototype.silence = function() {
   this._callbackSet.length = 0;
   this._out.disconnect();
   this._out = this._atop.ac.createGain();
-  this._out.gain.value = this.volume;
+  this._out.gain.value = this.gain;
   this._out.connect(this._atop.out);
 }
 Instrument.prototype.now = function() {
@@ -510,7 +562,7 @@ Instrument.prototype.now = function() {
     return this._now;
   }
   this._startPollTimer(true);
-  this._now = this._atop.ac.currentTime;
+  this._now = this._atop.ac.currentTime + Instrument.nowDelay;
   return this._now;
 }
 Instrument.prototype.on = function(ev, cb) {
@@ -539,7 +591,7 @@ Instrument.prototype._trigger = function(ev, record) {
   if (!cb) {
     return;
   }
-  for (var j = 0; j < cb.length; ++j) {
+  for (j = 0; j < cb.length; ++j) {
     cb[j](record);
   }
 }
@@ -550,7 +602,7 @@ function makeRecordRelease(instrument, record) {
       record.duration = Math.max(0, now - record.time);
       if (record.g) {
         var timbre = record.timbre || instrument._timbre,
-            cleanuptime = now + timbre.release;
+            cleanuptime = now + timbre.release + Instrument.cleanupDelay;
         record.g.gain.cancelScheduledValues(now);
         record.g.gain.linearRampToValueAtTime(0, cleanuptime);
         record.cleanuptime = cleanuptime;
@@ -558,7 +610,7 @@ function makeRecordRelease(instrument, record) {
     }
   });
 }
-Instrument.prototype.makeNoteSound = function(record) {
+Instrument.prototype._makeNoteSound = function(record) {
   if (record.duration <= 0) {
     return;
   }
@@ -567,7 +619,8 @@ Instrument.prototype.makeNoteSound = function(record) {
       releasetime = starttime + record.duration,
       attacktime = Math.min(releasetime, starttime + timbre.attack),
       stoptime = releasetime + timbre.release,
-      amp = timbre.volume * record.velocity,
+      doubled = timbre.detune && timbre.detune != 1.0,
+      amp = timbre.gain * record.velocity * (doubled ? 0.5 : 1.0),
       ac = this._atop.ac,
       g, f, o;
   g = ac.createGain();
@@ -578,7 +631,7 @@ Instrument.prototype.makeNoteSound = function(record) {
       Math.exp((attacktime - releasetime) / timbre.decay)), releasetime);
   g.gain.linearRampToValueAtTime(0, stoptime);
   g.connect(this._out);
-  if (this.cutoff == Infinity) {
+  if (!timbre.cutoff || timbre.cutoff == Infinity) {
     f = g;
   } else {
     f = ac.createBiquadFilter();
@@ -592,7 +645,7 @@ Instrument.prototype.makeNoteSound = function(record) {
   o.connect(f);
   o.start(starttime);
   o.stop(stoptime);
-  if (timbre.detune && timbre.detune != 1.0) {
+  if (doubled) {
     o2 = ac.createOscillator();
     o2.type = timbre.wave;
     o2.frequency.value = record.frequency * timbre.detune;
@@ -630,7 +683,7 @@ Instrument.prototype._doPoll = function() {
     if (j > 0) {
       work = this._queue.splice(0, j);
       for (j = 0; j < work.length; ++j) {
-        this.makeNoteSound(work[j]);
+        this._makeNoteSound(work[j]);
       }
       this._minQueueTime =
         (this._queue.length > 0) ? this._queue[0].time : Infinity;
@@ -675,7 +728,7 @@ Instrument.prototype._doPoll = function() {
   // Notify about any stems starting.
   for (j = 0; j < this._startSet.length; ++j) {
     if (this._startSet[j].time <= now) {
-      this._trigger('stemstart', this._startSet[j]);
+      this._trigger('notestart', this._startSet[j]);
       this._finishSet.push(this._startSet[j]);
       this._startSet.splice(j, 1);
       j -= 1;
@@ -719,12 +772,19 @@ Instrument.prototype._startPollTimer = function(soon) {
       function() { instrument._doPoll(); }, delay * 1000);
 }
 Instrument.prototype.tone = function(pitch, velocity, duration, delay, timbre) {
-  if (!pitch) {
-    frequency = pitchToFrequency('C');
-  } else if (isNaN(pitch)) {
-    frequency = pitchToFrequency(pitch);
+  var midi, frequency;
+  if (!pitch) { pitch = 'C'; }
+  if (isNaN(pitch)) {
+    midi = pitchToMidi(pitch);
+    frequency = midiToFrequency(midi);
   } else {
     frequency = Number(pitch);
+    if (frequency < 0) {
+      midi = -frequency;
+      frequency = midiToFrequency(midi);
+    } else {
+      midi = frequencyToMidi(frequency);
+    }
   }
   var ac = this._atop.ac,
       now = this.now(),
@@ -733,6 +793,7 @@ Instrument.prototype.tone = function(pitch, velocity, duration, delay, timbre) {
         time: time,
         on: false,
         frequency: frequency,
+        midi: midi,
         velocity: (velocity == null ? 1 : velocity),
         duration: (duration == null ? Instrument.toneLength : duration),
         timbre: timbre,
@@ -741,7 +802,7 @@ Instrument.prototype.tone = function(pitch, velocity, duration, delay, timbre) {
         cleanuptime: Infinity
       };
   if (time < now + Instrument.bufferSecs) {
-    this.makeNoteSound(record);
+    this._makeNoteSound(record);
   } else {
     if (!this._unsortedQueue && this._queue.length &&
         time < this._queue[this._queue.length -1].time) {
@@ -754,13 +815,13 @@ Instrument.prototype.tone = function(pitch, velocity, duration, delay, timbre) {
   return { release: makeRecordRelease(this, record) };
 }
 Instrument.prototype.schedule = function(delay, callback) {
-  this._callbackSet.push(this.now() + delay, callback);
+  this._callbackSet.push({ time: this.now() + delay, callback: callback });
 }
 Instrument.prototype.play = function(abcstring) {
   var args = Array.prototype.slice.call(arguments),
       done = null,
       opts = {},
-      abcfile, argindex, tempo, timbre, k, delay, maxdelay = 0,
+      abcfile, argindex, tempo, timbre, k, delay, maxdelay = 0, attenuate,
       voicename, stems, ni, j, stem, note, beatsecs, secs, files = [];
   if (args.length && $.isFunction(args[args.length - 1])) {
     done = args.pop();
@@ -772,9 +833,13 @@ Instrument.prototype.play = function(abcstring) {
   }
   for (; argindex < args.length; ++argindex) {
     abcfile = parseABCFile(args[argindex]);
+    console.log(abcfile);
     if (!abcfile) continue;
     if (!opts.tempo && abcfile.tempo) {
       opts.tempo = abcfile.tempo;
+      if (abcfile.unitbeat) {
+        opts.tempo *= abcfile.unitbeat / (abcfile.unitnote || 1);
+      }
     }
     if (!abcfile.voice) continue;
     files.push(abcfile);
@@ -790,6 +855,9 @@ Instrument.prototype.play = function(abcstring) {
       delay = 0;
       for (ni = 0; ni < stems.length; ++ni) {
         stem = stems[ni];
+        // Attenuate chords to reduce clipping.
+        attenuate = 1 / Math.sqrt(stem.note.length);
+        attenuate = 1 / stem.note.length;
         for (j = 0; j < stem.note.length; ++j) {
           note = stem.note[j];
           if (note.holdover) {
@@ -802,7 +870,7 @@ Instrument.prototype.play = function(abcstring) {
           }
           this.tone(
             note.pitch,
-            note.velocity || 1,
+            note.velocity || attenuate,
             secs,
             delay,
             timbre);
@@ -812,8 +880,10 @@ Instrument.prototype.play = function(abcstring) {
       maxdelay = Math.max(delay, maxdelay);
     }
   }
+  this._maxScheduledTime =
+      Math.max(this._maxScheduledTime, this.now() + maxdelay);
   if (done) {
-    schedule(maxdelay, done);
+    this.schedule(maxdelay, done);
   }
 }
 
